126788246bec60249392a07f57d76b4e506266d0
riveralights <riveralights@gmail.com>
2021-08-06 09:11:21 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('#hamburger').addEventListener('click', () => {
+    document.querySelector('#menu').classList.toggle('hidden');
+    document.querySelector('#navButton').classList.toggle('hidden');
+});
